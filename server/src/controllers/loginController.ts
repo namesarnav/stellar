ad9fa3307908cda65f7d4ae8758c46de1dfa2de3
Arnav Verma <arnavverma522@gmail.com>
2025-10-06 04:50:47 -0500

    test: login

--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -9,7 +9,7 @@ export async function loginController(req: Request, res: Response) {
     if (email === "admin@admin.com" && password === "admin123") {
       res.status(200).json({ message: "Login successful" });
     } else {
-      res.status(401).json({ error: "Invalid email or password" });
+      res.status(200).json({ message: "Unauthenticated" });
     }
   } else {
     res.status(400).json({ error: "Invalid email or password" });
