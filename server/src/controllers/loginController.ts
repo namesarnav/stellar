88a2c552e751c22930e5f5bff62fea2bbf6ffd59
Arnav Verma <arnavverma522@gmail.com>
2025-10-06 14:29:08 -0500

    login

--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from "express";
 
-export async function loginController(req: Request, res: Response) {
-  // Your login logic here
-  let { email, password } = req.body;
+function loginController(req: Request, res: Response) {
+  const { email, password } = req.body;
 
-  if (email && password) {
-    // Your login logic here
-    if (email === "admin@admin.com" && password === "admin123") {
-      res.status(200).json({ message: "Login successful" });
-    } else {
-      res.status(200).json({ message: "Unauthenticated" });
-    }
-  } else {
-    res.status(400).json({ error: "Invalid email or password" });
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required" });
+  } else if (!email.includes("@")) {
+    return res.status(400).json({ error: "Invalid email format" });
+  } else if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 8 characters long" });
   }
 }
