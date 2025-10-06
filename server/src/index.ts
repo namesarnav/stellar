3405d42d0ac112a1eb0fbaca81c4404c909b9089
Arnav Verma <arnavverma522@gmail.com>
2025-10-06 04:44:35 -0500

    login changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import healthRouter from "./routes/health";
 import usersRouter from "./routes/users";
+import loginRouter from "./routes/login";
 
 const app = express();
 const PORT = 3000;
@@ -10,8 +11,11 @@ app.use(express.json()); // parse JSON
 // // routes
 app.use("/health", healthRouter);
 app.use("/users", usersRouter);
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
+  // Render Home Page Here.
+
   res.send("Hello World!");
 });
 
