3a89daf8f0a22783ebe78d2416e5ee15150eff9a
Arnav Verma <arnavverma522@gmail.com>
2025-10-06 04:43:33 -0500

    Add local login controller

---
+++ server/src/controllers/loginController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export async function loginController(req: Request, res: Response) {
+  // Your login logic here
+  let { email, password } = req.body;
+
+  if (email && password) {
+    // Your login logic here
+    if (email === "admin@admin.com" && password === "admin123") {
+      res.status(200).json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ error: "Invalid email or password" });
+    }
+  } else {
+    res.status(400).json({ error: "Invalid email or password" });
+  }
+}
